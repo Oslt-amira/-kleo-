de9dc87ba22206e63e2334babf5ed540f1e6fb44
Oslt-amira <107197253+Oslt-amira@users.noreply.github.com>
2025-05-15 19:07:57 +0100

    Scroll Velocity fix

--- src/components/landing-page/ScrollVelocity.tsx
+++ src/components/landing-page/ScrollVelocity.tsx
@@ -135,7 +135,7 @@ export const ScrollVelocity: React.FC<ScrollVelocityProps> = ({
       baseX.set(baseX.get() + moveBy);
     });
 
-    const spans = [];
+    const spans: React.ReactElement[] = [];
     for (let i = 0; i < numCopies!; i++) {
       spans.push(
         <span
